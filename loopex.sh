c355ca6e91ddb3fa20fc0d28c491c36b4f5a001f
root <root@VAX750.attlocal.net>
2020-03-04 20:15:22 -0600

    First example shell script

---
+++ loopex.sh
@@ -0,0 +1,13 @@
+#Simple loop
+for i in {1..5}
+do
+	echo "Welcome $i times"
+	echo "The current time is ";date
+	sleep 2
+done
+#Count by 2 from 0 to 10
+for i in {0..10..2}
+do
+	echo "Welcome to iteration $i"
+	sleep 2
+done
